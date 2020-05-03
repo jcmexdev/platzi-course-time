ec6e8728e2d7840c748244768e472a69bf25d23b
efrenmartinez <developer.emartinez@gmail.com>
2020-05-02 22:54:27 -0500

    [FEATURE] Expresión regex para validar una URL

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const prompt = require('prompt');
-const handleRequest = require('./src/handleRequest');
+const prompt = require("prompt");
+const handleRequest = require("./src/handleRequest");
 
-const promptAttributes = [{ name: 'platziURL', required: true }];
-prompt.get(promptAttributes, function(error, result) {
+const promptAttributes = [
+  {
+    name: "platziURL",
+    required: true,
+    description: "URL del curso",
+    type: "string",
+    pattern: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+    message: "La URL es incorrecta.",
+  },
+];
+prompt.get(promptAttributes, (error, result) => {
   if (error) {
     console.error(error);
   }
-  console.log('Obteniendo información, espera un momento por favor.....');
+  console.log("Obteniendo información, espera un momento por favor.....");
   handleRequest(result.platziURL);
 });
 
